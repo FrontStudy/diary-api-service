3e75d92c3f7f98091d74950dbbc87fca069dec72
6cessfuldev <6cessfuldev@gmail.com>
2024-03-21 21:22:41 +0900

    admin/memberList 검색 시 email, nickname, name 은 부분 일치 검색

--- src/main/java/com/yuk/wazzangstudyrestapi1/repositorys/MemberSpecifications.java
+++ src/main/java/com/yuk/wazzangstudyrestapi1/repositorys/MemberSpecifications.java
@@ -10,11 +10,11 @@ public class MemberSpecifications {
         return (root, query, criteriaBuilder) -> {
             return criteriaBuilder.and(
                     requestMemberListDto.getMemberId() != null ? criteriaBuilder.equal(root.get("id"), requestMemberListDto.getMemberId()) : criteriaBuilder.conjunction(),
-                    requestMemberListDto.getEmail() != null ? criteriaBuilder.like(root.get("email"), requestMemberListDto.getEmail()) : criteriaBuilder.conjunction(),
+                    requestMemberListDto.getEmail() != null ? criteriaBuilder.like(root.get("email"), "%" + requestMemberListDto.getEmail() + "%") : criteriaBuilder.conjunction(),
                     requestMemberListDto.getUserrole() != null ? criteriaBuilder.equal(root.get("userrole"), requestMemberListDto.getUserrole()) : criteriaBuilder.conjunction(),
-                    requestMemberListDto.getNickname() != null ? criteriaBuilder.like(root.get("nickname"), requestMemberListDto.getNickname()) : criteriaBuilder.conjunction(),
+                    requestMemberListDto.getNickname() != null ? criteriaBuilder.like(root.get("nickname"), "%" + requestMemberListDto.getNickname() + "%") : criteriaBuilder.conjunction(),
                     requestMemberListDto.getBirthDate() != null ? criteriaBuilder.equal(root.get("birthDate"), requestMemberListDto.getBirthDate()) : criteriaBuilder.conjunction(),
-                    requestMemberListDto.getName() != null ? criteriaBuilder.like(root.get("name"), requestMemberListDto.getName()) : criteriaBuilder.conjunction(),
+                    requestMemberListDto.getName() != null ? criteriaBuilder.like(root.get("name"), "%" + requestMemberListDto.getName() + "%") : criteriaBuilder.conjunction(),
                     requestMemberListDto.getGender() != null ? criteriaBuilder.equal(root.get("gender"), requestMemberListDto.getGender()) : criteriaBuilder.conjunction(),
                     requestMemberListDto.getActive() != null ? criteriaBuilder.equal(root.get("active"), requestMemberListDto.getActive()) : criteriaBuilder.conjunction()
             );
