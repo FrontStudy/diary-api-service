c4fa94788b232b09b522f40422ef6708175e4251
6cessfuldev <6cessfuldev@gmail.com>
2024-03-21 20:09:42 +0900

    DiaryResponseDto가 BaseTImeEntity 상속 관계 설정 및 생성자 수정

--- src/main/java/com/yuk/wazzangstudyrestapi1/dtos/diary/DiaryResponseDto.java
+++ src/main/java/com/yuk/wazzangstudyrestapi1/dtos/diary/DiaryResponseDto.java
@@ -2,17 +2,20 @@ package com.yuk.wazzangstudyrestapi1.dtos.diary;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.yuk.wazzangstudyrestapi1.domains.Diary;
+import com.yuk.wazzangstudyrestapi1.dtos.BaseTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public class DiaryResponseDto {
+public class DiaryResponseDto extends BaseTimeEntity {
 
     private Long id;
     private Long memberId;
@@ -22,9 +25,21 @@ public class DiaryResponseDto {
     private String accessLevel;
     private Boolean active;
 
+    public DiaryResponseDto(LocalDateTime createdDate, LocalDateTime modifiedDate, Long id, Long memberId, String title, String content, Long imgid, String accessLevel, Boolean active) {
+        super(createdDate, modifiedDate);
+        this.id = id;
+        this.memberId = memberId;
+        this.title = title;
+        this.content = content;
+        this.imgid = imgid;
+        this.accessLevel = accessLevel;
+        this.active = active;
+    }
 
     public static DiaryResponseDto from(Diary diary) {
         return new DiaryResponseDto(
+                diary.getCreatedDate(),
+                diary.getModifiedDate(),
                 diary.getId(),
                 diary.getMemberId(),
                 diary.getTitle(),
